51b1fbad208ccba0df475d6fb9e95ddfbb64227d
linsyd <linsy.joyner@gmail.com>
2018-11-22 14:20:20 -0700

    post book chapter every second

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 const fetch = require('node-fetch');
 const TOKEN = require('./config.js');
-const book = require('./book');
+const bookExcerpts = require('./book-excerpts');
+const cron = require('node-cron');
 
-const postToSlack = () => {
+const sendExcerptToSlack = excerptToPost => fetch('https://slack.com/api/chat.postMessage', {
+  method: 'post',
+  body: JSON.stringify({
+    channel: '#the-war-of-art',
+    text: excerptToPost
+  }),
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${TOKEN}`
+  },
+})
+  .then(res => res.json())
+  .then(res => console.log('res ', res))
+  .catch(error => console.log('error ', error))
 
-  const excerptToPost = book[0].text
+let posts = [];
 
-  fetch('https://slack.com/api/chat.postMessage', {
-    method: 'post',
-    body: JSON.stringify({
-      channel: '#the-war-of-art',
-      text: excerptToPost
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${TOKEN}`
-    },
-  })
-    .then(res => res.json())
-    .then(res => console.log('res ', res))
-    .catch(error => console.log('error ', error))
+cron.schedule('* * * * * *', () => {
 
-}
+  // fetch posts from database
 
-postToSlack();
+  const bookFinished = posts.length === bookExcerpts.length;
+
+  if (!bookFinished) {
+    console.log('posts starting value ', posts);
+
+    const excerpt = bookExcerpts[posts.length].text
+    sendExcerptToSlack(excerpt)
+
+    let newPosts = [...posts, 'chapter ' + (posts.length + 1)]
+    console.log('newPosts ', newPosts);
+
+    posts = newPosts // update posts in database
+
+    console.log('--------------- \n')
+  } else {
+    process.exit();
+  }
+});
