b7d93576cfcf7828a06dbcd714efbc3035c45704
linsyd <linsy.joyner@gmail.com>
2018-11-22 12:31:18 -0700

    add mock data

---
+++ book.js
@@ -0,0 +1,19 @@
+const book = [
+  {
+    chapter: 1,
+    postAll: true,
+    text: 'chapter one text'
+  },
+  {
+    chapter: 2,
+    postAll: true,
+    text: 'chapter two text'
+  },
+  {
+    chapter: 3,
+    postAll: true,
+    text: 'chapter three text'
+  },
+]
+
+module.exports = book
